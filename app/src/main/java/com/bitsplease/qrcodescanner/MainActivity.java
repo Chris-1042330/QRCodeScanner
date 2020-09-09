058b22ca943ab0dfd1ebbd88afa06cc919a675de
Caffeine Boy <christian.aartman@gmail.com>
2020-09-09 14:51:07 +0200

    visual adjustmens and scanner capabilities

--- app/src/main/java/com/bitsplease/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/bitsplease/qrcodescanner/MainActivity.java
@@ -15,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void openScanner(View view){
         Intent intent = new Intent(this, ScanActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/bitsplease/qrcodescanner/ScanActivity.java
+++ app/src/main/java/com/bitsplease/qrcodescanner/ScanActivity.java
@@ -1,14 +1,118 @@
 package com.bitsplease.qrcodescanner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class ScanActivity extends AppCompatActivity {
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+
+public class ScanActivity extends Activity {
+
+    TextView barcodeInfo;
+    SurfaceView cameraView;
+    CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Verwijder titel- en notificatiebar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_scan);
+
+        cameraView = findViewById(R.id.camera_view);
+        barcodeInfo =  findViewById(R.id.txtContent);
+
+
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.QR_CODE)//QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    public void closeScanner(View view){
+        this.onBackPressed();
     }
 }
